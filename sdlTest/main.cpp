3dff5a3c78555c3890270f370f94f51029486263
ljlcarrien <ljlcarrien@icloud.com>
2019-08-13 00:42:22 +0800

    第1课：Hello World https://www.willusher.io/sdl2%20tutorials/2013/08/17/lesson-1-hello-world

--- sdlTest/main.cpp
+++ sdlTest/main.cpp
@@ -3,39 +3,66 @@
 
 #include "SDL.h"
 #include <stdio.h>
+#include <iostream>
 
 int main(int argc, char* argv[]) {
 
-	SDL_Window *window;                    // Declare a pointer
 
-	// 07-14-2018: Modified by ZDH
-	// SDL_Init(SDL_INIT_VIDEO);              // Initialize SDL2
-	SDL_Init(SDL_INIT_EVERYTHING);              // Initialize SDL2
-										   // Create an application window with the following settings:
-	window = SDL_CreateWindow(
-		"An SDL2 window",                  // window title
-		SDL_WINDOWPOS_UNDEFINED,           // initial x position
-		SDL_WINDOWPOS_UNDEFINED,           // initial y position
-		640,                               // width, in pixels
-		480,                               // height, in pixels
-		SDL_WINDOW_OPENGL                  // flags - see below
-	);
-
-	// Check that the window was successfully created
-	if (window == NULL) {
-		// In the case that the window could not be made...
-		printf("Could not create window: %s\n", SDL_GetError());
+	if (SDL_Init(SDL_INIT_VIDEO) != 0) {
+		std::cout << "SDL_Init Error: " << SDL_GetError() << std::endl;
 		return 1;
 	}
 
-	// The window is open: could enter program loop here (see SDL_PollEvent())
+	SDL_Window* win = SDL_CreateWindow("Hello World!", 100, 100, 640, 480, SDL_WINDOW_SHOWN);
+	if (win == nullptr) {
+		std::cout << "SDL_CreateWindow Error: " << SDL_GetError() << std::endl;
+		SDL_Quit();
+		return 1;
+	}
 
-	SDL_Delay(3000);  // Pause execution for 3000 milliseconds, for example
+	SDL_Renderer* ren = SDL_CreateRenderer(win, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
+	if (ren == nullptr) {
+		SDL_DestroyWindow(win);
+		std::cout << "SDL_CreateRenderer Error: " << SDL_GetError() << std::endl;
+		SDL_Quit();
+		return 1;
+	}
 
-	// Close and destroy the window
-	SDL_DestroyWindow(window);
+	std::string imagePath = "E:\\learn_SDL\\Debug\\hello.bmp";
+	SDL_Surface* bmp = SDL_LoadBMP(imagePath.c_str());
+	if (bmp == nullptr) {
+		SDL_DestroyRenderer(ren);
+		SDL_DestroyWindow(win);
+		std::cout << "SDL_LoadBMP Error: " << SDL_GetError() << std::endl;
+		SDL_Quit();
+		return 1;
+	}
 
-	// Clean up
+	SDL_Texture* tex = SDL_CreateTextureFromSurface(ren, bmp);
+	SDL_FreeSurface(bmp);
+	if (tex == nullptr) {
+		SDL_DestroyRenderer(ren);
+		SDL_DestroyWindow(win);
+		std::cout << "SDL_CreateTextureFromSurface Error: " << SDL_GetError() << std::endl;
+		SDL_Quit();
+		return 1;
+	}
+
+	//A sleepy rendering loop, wait for 3 seconds and render and present the screen each time
+	for (int i = 0; i < 3; ++i) {
+		//First clear the renderer
+		SDL_RenderClear(ren);
+		//Draw the texture
+		SDL_RenderCopy(ren, tex, NULL, NULL);
+		//Update the screen
+		SDL_RenderPresent(ren);
+		//Take a quick break after all that hard work
+		SDL_Delay(1000);
+	}
+
+	SDL_DestroyTexture(tex);
+	SDL_DestroyRenderer(ren);
+	SDL_DestroyWindow(win);
 	SDL_Quit();
 	return 0;
 }
