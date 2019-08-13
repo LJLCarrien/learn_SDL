ea2c4796ceefc02470fcb1802f90d5c810819e48
ljlcarrien <ljlcarrien@icloud.com>
2019-08-13 23:49:50 +0800

    后记1:Easy Cleanup 第2课：Don't Put Everything in Main https://www.willusher.io/sdl2%20tutorials/2013/08/17/lesson-1-hello-world

--- sdlTest/main.cpp
+++ sdlTest/main.cpp
@@ -1,21 +1,115 @@
 // Example program:
 // Using SDL2 to create an application window
 
+#ifndef CLEANUP_H
+#define CLEANUP_H
+
+#include <utility>
+#include <SDL.h>
+
+/*
+ * Recurse through the list of arguments to clean up, cleaning up
+ * the first one in the list each iteration.
+ */
+template<typename T, typename... Args>
+void cleanup(T* t, Args&& ... args) {
+	//Cleanup the first item in the list
+	cleanup(t);
+	//Recurse to clean up the remaining arguments
+	cleanup(std::forward<Args>(args)...);
+}
+/*
+ * These specializations serve to free the passed argument and also provide the
+ * base cases for the recursive call above, eg. when args is only a single item
+ * one of the specializations below will be called by
+ * cleanup(std::forward<Args>(args)...), ending the recursion
+ * We also make it safe to pass nullptrs to handle situations where we
+ * don't want to bother finding out which values failed to load (and thus are null)
+ * but rather just want to clean everything up and let cleanup sort it out
+ */
+template<>
+inline void cleanup<SDL_Window>(SDL_Window* win) {
+	if (!win) {
+		return;
+	}
+	SDL_DestroyWindow(win);
+}
+template<>
+inline void cleanup<SDL_Renderer>(SDL_Renderer* ren) {
+	if (!ren) {
+		return;
+	}
+	SDL_DestroyRenderer(ren);
+}
+template<>
+inline void cleanup<SDL_Texture>(SDL_Texture* tex) {
+	if (!tex) {
+		return;
+	}
+	SDL_DestroyTexture(tex);
+}
+template<>
+inline void cleanup<SDL_Surface>(SDL_Surface* surf) {
+	if (!surf) {
+		return;
+	}
+	SDL_FreeSurface(surf);
+}
+
+#endif
+
 #include "SDL.h"
 #include <stdio.h>
 #include <iostream>
+
+
+const int SCREEN_WIDTH = 640;
+const int SCREEN_HEIGHT = 480;
+
+void logSDLError(std::ostream& os, const std::string& msg) {
+	os << msg << " error: " << SDL_GetError() << std::endl;
+}
+
+void logSDLError(const std::string& msg) {
+	std::cout << msg << " error: " << SDL_GetError() << std::endl;
+}
+
+
+SDL_Texture* loadTexture(const std::string& file, SDL_Renderer* ren) {
+	SDL_Texture* texture = nullptr;
+	SDL_Surface* loadedImage = SDL_LoadBMP(file.c_str());
+	if (loadedImage != nullptr) {
+		texture = SDL_CreateTextureFromSurface(ren, loadedImage);
+		SDL_FreeSurface(loadedImage);
+		if (texture == nullptr) {
+			logSDLError("CreateTextureFromSurface");
+		}
+	}
+	else {
+		logSDLError("LoadBMP");
+	}
+	return texture;
+}
+
+void renderTexture(SDL_Texture* tex, SDL_Renderer* ren, int x, int y) {
+	SDL_Rect dst;
+	dst.x = x;
+	dst.y = y;
+	SDL_QueryTexture(tex, NULL, NULL, &dst.w, &dst.h);
+	SDL_RenderCopy(ren, tex, NULL, &dst);
+}
 
 int main(int argc, char* argv[]) {
 
 
 	if (SDL_Init(SDL_INIT_VIDEO) != 0) {
-		std::cout << "SDL_Init Error: " << SDL_GetError() << std::endl;
+		logSDLError("SDL_Init");
 		return 1;
 	}
 
 	SDL_Window* win = SDL_CreateWindow("Hello World!", 100, 100, 640, 480, SDL_WINDOW_SHOWN);
 	if (win == nullptr) {
-		std::cout << "SDL_CreateWindow Error: " << SDL_GetError() << std::endl;
+		logSDLError("SDL_CreateWindow");
 		SDL_Quit();
 		return 1;
 	}
@@ -23,27 +117,15 @@ int main(int argc, char* argv[]) {
 	SDL_Renderer* ren = SDL_CreateRenderer(win, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
 	if (ren == nullptr) {
 		SDL_DestroyWindow(win);
-		std::cout << "SDL_CreateRenderer Error: " << SDL_GetError() << std::endl;
+		logSDLError("SDL_CreateRenderer");
 		SDL_Quit();
 		return 1;
 	}
 
-	std::string imagePath = "E:\\learn_SDL\\Debug\\hello.bmp";
-	SDL_Surface* bmp = SDL_LoadBMP(imagePath.c_str());
-	if (bmp == nullptr) {
-		SDL_DestroyRenderer(ren);
-		SDL_DestroyWindow(win);
-		std::cout << "SDL_LoadBMP Error: " << SDL_GetError() << std::endl;
-		SDL_Quit();
-		return 1;
-	}
-
-	SDL_Texture* tex = SDL_CreateTextureFromSurface(ren, bmp);
-	SDL_FreeSurface(bmp);
-	if (tex == nullptr) {
-		SDL_DestroyRenderer(ren);
-		SDL_DestroyWindow(win);
-		std::cout << "SDL_CreateTextureFromSurface Error: " << SDL_GetError() << std::endl;
+	SDL_Texture *background = loadTexture("background.bmp", ren);
+	SDL_Texture *image = loadTexture("image.bmp", ren);
+	if (background == nullptr || image == nullptr) {
+		cleanup(background, image, ren, win);
 		SDL_Quit();
 		return 1;
 	}
@@ -52,17 +134,25 @@ int main(int argc, char* argv[]) {
 	for (int i = 0; i < 3; ++i) {
 		//First clear the renderer
 		SDL_RenderClear(ren);
-		//Draw the texture
-		SDL_RenderCopy(ren, tex, NULL, NULL);
-		//Update the screen
+		
+		int bW, bH;
+		SDL_QueryTexture(background, NULL, NULL, &bW, &bH);
+		renderTexture(background, ren, 0, 0);
+		renderTexture(background, ren, bW, 0);
+		renderTexture(background, ren, 0, bH);
+		renderTexture(background, ren, bW, bH);
+
+		int iW, iH;
+		SDL_QueryTexture(image, NULL, NULL, &iW, &iH);
+		int x = SCREEN_WIDTH / 2 - iW / 2;
+		int y = SCREEN_HEIGHT / 2 - iH / 2;
+		renderTexture(image, ren, x, y);
+
 		SDL_RenderPresent(ren);
-		//Take a quick break after all that hard work
 		SDL_Delay(1000);
 	}
 
-	SDL_DestroyTexture(tex);
-	SDL_DestroyRenderer(ren);
-	SDL_DestroyWindow(win);
+	cleanup(background, image, ren, win);
 	SDL_Quit();
 	return 0;
 }
