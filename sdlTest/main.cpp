098d93df157bbd895375ebeecdeaf3ef71c46d3c
ljlcarrien <ljlcarrien@icloud.com>
2019-08-21 00:45:34 +0800

    lession 6 Extension Libraries and Loading Other Image Formats http://lazyfoo.net/tutorials/SDL/06_extension_libraries_and_loading_other_image_formats/index.php
    Loading PNGs with SDL_image
    http://lazyfoo.net/tutorials/SDL/06_extension_libraries_and_loading_other_image_formats/index2.php

--- sdlTest/main.cpp
+++ sdlTest/main.cpp
@@ -18,6 +18,7 @@ void cleanup(T* t, Args&& ... args) {
 	//Recurse to clean up the remaining arguments
 	cleanup(std::forward<Args>(args)...);
 }
+
 /*
  * These specializations serve to free the passed argument and also provide the
  * base cases for the recursive call above, eg. when args is only a single item
@@ -64,10 +65,10 @@ inline void cleanup<SDL_Surface>(SDL_Surface* surf) {
 #include "SDL_image.h"
 #include "SDL_ttf.h"
 
-//#pragma comment(lib ,"sdl2.lib")
+//#pragma comment(lib ,"SDL2.lib")
 //#pragma comment(lib ,"SDL2main.lib")
-#pragma comment(lib ,"SDL2_image.lib")
-#pragma comment(lib ,"SDL2_ttf.lib")
+//#pragma comment(lib ,"SDL2_image.lib")
+//#pragma comment(lib ,"SDL2_ttf.lib")
 
 
 const int SCREEN_WIDTH = 640;
@@ -78,11 +79,15 @@ const int TILE_SIZE = 40;
 void close();
 
 void logSDLError(std::ostream& os, const std::string& msg) {
-	os << msg << " error: " << SDL_GetError() << std::endl;
+	os << msg << "[SDL_GetError] error: " << SDL_GetError() << std::endl;
 }
 
 void logSDLError(const std::string& msg) {
-	std::cout << msg << " error: " << SDL_GetError() << std::endl;
+	std::cout << msg << "[SDL_GetError] error: " << SDL_GetError() << std::endl;
+}
+
+void logIMGError(const std::string& msg) {
+	std::cout << msg << "[IMG_GetError] error: " << IMG_GetError() << std::endl;
 }
 
 
@@ -157,10 +162,11 @@ SDL_Surface* loadSurface(std::string path)
 	SDL_Surface* optimizedSurface = NULL;
 
 	//Load image at specified path
-	SDL_Surface* loadedSurface = SDL_LoadBMP(path.c_str());
+	SDL_Surface* loadedSurface = IMG_Load(path.c_str());
 	if (loadedSurface == NULL)
 	{
-		printf("Unable to load image %s! SDL Error: %s\n", path.c_str(), SDL_GetError());
+		//Unable to load image
+		logIMGError("IMG_Load");
 	}
 	else
 	{
@@ -168,7 +174,8 @@ SDL_Surface* loadSurface(std::string path)
 		optimizedSurface = SDL_ConvertSurface(loadedSurface, gScreenSurface->format, 0);
 		if (optimizedSurface == NULL)
 		{
-			printf("Unable to optimize image %s! SDL Error: %s\n", path.c_str(), SDL_GetError());
+			logSDLError("SDL_ConvertSurface");
+			//printf("Unable to optimize image %s! SDL Error: %s\n", path.c_str(), SDL_GetError());
 		}
 
 		//Get rid of old loaded surface
@@ -184,7 +191,7 @@ bool loadMedia()
 	bool success = true;
 
 	//Load stretching surface
-	gStretchedSurface = loadSurface("stretch.bmp");
+	gStretchedSurface = loadSurface("preview.png");
 	if (gStretchedSurface == NULL)
 	{
 		printf("Failed to load stretching image!\n");
@@ -208,26 +215,49 @@ bool InitRender(SDL_Window* window) {
 
 }
 
-int main(int argc, char* argv[]) {
+bool init()
+{
+	//Initialization flag
+	bool success = true;
 
-
-	if (SDL_Init(SDL_INIT_VIDEO) != 0) {
+	//Initialize SDL
+	if (SDL_Init(SDL_INIT_VIDEO) < 0)
+	{
 		logSDLError("SDL_Init");
-		return 1;
+		return false;
 	}
 	if (TTF_Init() != 0) {
 		logSDLError(std::cout, "TTF_Init");
-		SDL_Quit();
-		return 1;
+		return false;
 	}
-	win = SDL_CreateWindow("Hello World!", 100, 100, 640, 480, SDL_WINDOW_SHOWN);
-	if (win == nullptr) {
+	//Create window
+	win = SDL_CreateWindow("SDL Tutorial", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
+	if (win == NULL)
+	{
 		logSDLError("SDL_CreateWindow");
-		SDL_Quit();
-		return 1;
+		return false;
 	}
-	else {
+	//Initialize PNG loading
+	int imgFlags = IMG_INIT_PNG;
+	if (!(IMG_Init(imgFlags) & imgFlags))
+	{
+		logIMGError("IMG_Init");
+		return false;
+	}
+	else
+	{
+		//Get window surface
 		gScreenSurface = SDL_GetWindowSurface(win);
+	}
+	return success;
+}
+
+int main(int argc, char* argv[]) {
+
+	if (init())
+	{
+		
+
 	}
 
 	if (!loadMedia())
