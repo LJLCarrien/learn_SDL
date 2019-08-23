089a3812f157cd598500468d442f84e6f867e448
ljlcarrien <ljlcarrien@icloud.com>
2019-08-24 02:05:06 +0800

    lession 8 Geometry Rendering http://lazyfoo.net/tutorials/SDL/08_geometry_rendering/index.php

--- sdlTest/main.cpp
+++ sdlTest/main.cpp
@@ -217,13 +217,13 @@ bool loadMedia()
 	//Loading success flag
 	bool success = true;
 
-	gTexture=lazyFoo_loadTexture("lession7.png", gRenderer);
+	/*gTexture=lazyFoo_loadTexture("lession7.png", gRenderer);
 
 	if (gTexture == NULL)
 	{
 		printf("Failed to load stretching image!\n");
 		success = false;
-	}
+	}*/
 	return success;
 }
 
@@ -307,12 +307,33 @@ int main(int argc, char* argv[]) {
 				quit = true;
 			}
 		}
-
 		//Clear screen
+		SDL_SetRenderDrawColor(gRenderer, 0xFF, 0xFF, 0xFF, 0xFF);
 		SDL_RenderClear(gRenderer);
 
-		//Render texture to screen
-		SDL_RenderCopy(gRenderer, gTexture, NULL, NULL);
+		//Render red filled quad
+		SDL_Rect fillRect = { SCREEN_WIDTH / 4, SCREEN_HEIGHT / 4, SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2 };
+		SDL_SetRenderDrawColor(gRenderer, 0xFF, 0x00, 0x00, 0xFF);
+		SDL_RenderFillRect(gRenderer, &fillRect);
+
+		//Render green outlined quad
+		SDL_Rect outlineRect = { SCREEN_WIDTH / 6, SCREEN_HEIGHT / 6, SCREEN_WIDTH * 2 / 3, SCREEN_HEIGHT * 2 / 3 };
+		SDL_SetRenderDrawColor(gRenderer, 0x00, 0xFF, 0x00, 0xFF);
+		SDL_RenderDrawRect(gRenderer, &outlineRect);
+
+		//Draw blue horizontal line
+		SDL_SetRenderDrawColor(gRenderer, 0x00, 0x00, 0xFF, 0xFF);
+		SDL_RenderDrawLine(gRenderer, 0, SCREEN_HEIGHT / 2, SCREEN_WIDTH, SCREEN_HEIGHT / 2);
+
+		//Draw vertical line of yellow dots
+		SDL_SetRenderDrawColor(gRenderer, 0xFF, 0xFF, 0x00, 0xFF);
+		for (int i = 0; i < SCREEN_HEIGHT; i += 4)
+		{
+			SDL_RenderDrawPoint(gRenderer, SCREEN_WIDTH / 2, i);
+		}
+
+		////Render texture to screen
+		//SDL_RenderCopy(gRenderer, gTexture, NULL, NULL);
 
 		//Update screen
 		SDL_RenderPresent(gRenderer);
